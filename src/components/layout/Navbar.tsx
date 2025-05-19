99949f3c0a5383226b7352e054175f835c7bc413
Juan Agustin Apaza <juanagustin7887@gmail.com>
2025-05-19 15:45:50 -0400

    modificacion del header

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -42,7 +42,7 @@ const Navbar: React.FC = () => {
   return (
     <header
       className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${
-        isScrolled || isMenuOpen ? 'bg-white shadow-md' : 'bg-transparent'
+        isScrolled || isMenuOpen ? 'bg-white shadow-md' : 'bg-white shadow-md'
       }`}
     >
       <div className="container-custom">
